b7cfdcbe6e3200f2fe0381a2d67846eb688613c9
nakkijorma <juuso.j.parikka@gmail.com>
2018-10-01 14:17:34 +0300

    inital commit

--- app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
+++ app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
@@ -2,16 +2,12 @@ package com.example.juuso.nukkumatti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class UniMaara extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_uni_maara);
     }
 }
