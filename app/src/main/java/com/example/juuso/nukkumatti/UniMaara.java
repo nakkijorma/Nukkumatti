d25784876f735ac0e4b5e81199728598242b985e
ollijantunen <32096381+ollijantunen@users.noreply.github.com>
2018-10-03 13:42:31 +0300

    CalendarView

--- app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
+++ app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
@@ -1,10 +1,12 @@
 package com.example.juuso.nukkumatti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class UniMaara extends AppCompatActivity {
+public class UniMaara extends AppCompatActivity implements View.OnClickListener {
     private String pvm; // pvm muotoa 22/05/2018
     private double tunnit; // tunnit pilkulla 7,5
     private Button buttonLaheta;
@@ -13,5 +15,14 @@ public class UniMaara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uni_maara);
+
+        buttonLaheta = findViewById(R.id.buttonLaheta);
+        buttonLaheta.setOnClickListener(this);
+    }
+    @Override
+
+    // Tämä kuuntelee Lähetä-nappia
+    public void onClick(View view) {
+        // TODO - Tähän tietojen tallennus listaan.
     }
 }
