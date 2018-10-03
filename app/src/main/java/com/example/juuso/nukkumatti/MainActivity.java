7219318c8d18f4f9ac1c0a8aaa363fb5c1f9b6f7
nakkijorma <juuso.j.parikka@gmail.com>
2018-10-03 10:46:56 +0300

    Merge branch 'master' of https://github.com/nakkijorma/Nukkumatti
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/juuso/nukkumatti/MainActivity.java
+++ app/src/main/java/com/example/juuso/nukkumatti/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.juuso.nukkumatti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO - testi2
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                Intent intent1 = new Intent(this, UniMaara.class);
+                startActivity(intent1);
+                break;
+            case R.id.button2:
+                Intent intent2 = new Intent(this, Tilastot.class);
+                startActivity(intent2);
+                break;
+            case R.id.button3:
+                Intent intent3 = new Intent(this, Historia.class);
+                startActivity(intent3);
+                break;
+            default:
+                break;
+
+    }
+
+
+    // TODO - testi2
 
     }
 }
