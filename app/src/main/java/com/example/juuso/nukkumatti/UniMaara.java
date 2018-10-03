ccf839e5408ceec54c2602621b185bee15ae7fda
ollijantunen <32096381+ollijantunen@users.noreply.github.com>
2018-10-03 13:12:23 +0300

    CalendarView

--- app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
+++ app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
@@ -2,8 +2,12 @@ package com.example.juuso.nukkumatti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UniMaara extends AppCompatActivity {
+    private String pvm; // pvm muotoa 22/05/2018
+    private double tunnit; // tunnit pilkulla 7,5
+    private Button buttonLaheta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
