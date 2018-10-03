5382bb410c40dd6ab5453c21795672bcd2fc62a7
ollijantunen <32096381+ollijantunen@users.noreply.github.com>
2018-10-03 19:07:41 +0300

    Kalenterin tieto -> pvm - Toimii

--- app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
+++ app/src/main/java/com/example/juuso/nukkumatti/UniMaara.java
@@ -1,20 +1,46 @@
 package com.example.juuso.nukkumatti;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UniMaara extends AppCompatActivity implements View.OnClickListener {
     private String pvm; // pvm muotoa 22/05/2018
+    private String pvmToast;
     private double tunnit; // tunnit pilkulla 7,5
     private Button buttonLaheta;
+    private CalendarView calendarView;
+    private long date;
+    private TextView naytaPVM;
+    private Toast toastTallenna;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uni_maara);
 
+        calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                pvm = dayOfMonth + "/" + (month + 1) + "/" + year; //kuukausi Tammikuu == 0
+                Long date = calendarView.getDate();
+                pvmToast = date.toString();
+                naytaPVM.setText(pvmToast + ", " + dayOfMonth + "/" + (month + 1) + "/" + year + ", " + pvm); // TODO - pvmToast näyttää millisekunnit nykyisellä hetkellä
+                Toast toastPVM = Toast.makeText(getApplicationContext(),pvm,Toast.LENGTH_LONG);
+                toastPVM.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.RIGHT, 0, 250);
+                toastPVM.show();
+            }
+        });
+
+        naytaPVM = findViewById(R.id.naytaPVM);
         buttonLaheta = findViewById(R.id.buttonLaheta);
         buttonLaheta.setOnClickListener(this);
     }
@@ -25,5 +51,11 @@ public class UniMaara extends AppCompatActivity implements View.OnClickListener 
     @Override
     public void onClick(View view) {
         // TODO - Tähän tietojen tallennus listaan.
+        /* Intent intent3 = new Intent(this, Historia.class);
+        startActivity(intent3); */
+
+        toastTallenna = Toast.makeText(getApplicationContext(), R.string.toast_tallenna, Toast.LENGTH_LONG);
+        toastTallenna.setGravity(Gravity.BOTTOM, 0, 0);
+        toastTallenna.show();
     }
 }
